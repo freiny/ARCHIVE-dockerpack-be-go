76a13c962ee3fb59b8c60d955a8e60992ea0f88c
name <freiny50@gmail.com>
2016-08-07 17:53:08 -0500

    Update dockerpack-be-go

--- src/main.go
+++ src/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("[ HelloWorld ]")
+	if os.Getenv("APP_ENVIRONMENT") == "dev" {
+		fmt.Println("\n****************** APP READY\n")
+	}
+
+	r := gin.Default()
+	r.GET("/", onRoot)
+
+	r.Run(":" + os.Getenv("HTTP_PORT"))
+}
+
+func onRoot(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "pong"})
 }
